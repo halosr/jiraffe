cd52cfcf0503b633ea2e3b16c1c2b9f79b839d52
Subhakanta Roul <papu.roul99@gmail.com>
2025-08-26 00:21:37 +0530

    update token utils

--- server/src/utils/token.ts
+++ server/src/utils/token.ts
@@ -1,10 +1,23 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-export function generateToken(payload: Record<string, any>){
+export function generateJwt(payload: Record<string, any>): string {
     const tokenSecret = process.env.TOKEN_SIGN_SECRET;
-    const tokenExpiresIn= Number(process.env.TOKEN_EXPIRE_DURATION!);
+    const tokenExpiresIn = Number(process.env.TOKEN_EXPIRE_DURATION!);
 
-    const token = jwt.sign(payload, tokenSecret!, {expiresIn: tokenExpiresIn})
+    const token = jwt.sign(payload, tokenSecret!, { expiresIn: tokenExpiresIn });
 
     return token;
+}
+
+export function extractJwt(token: string): JwtPayload | string {
+    const tokenSecret = process.env.TOKEN_SIGN_SECRET;
+    const decodedToken = jwt.verify(token, tokenSecret!);
+
+    return decodedToken;
+}
+
+export function verifyJwt(token: string): boolean {
+    const decodedToken = extractJwt(token);
+
+    return !!decodedToken;
 }
