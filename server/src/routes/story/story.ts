3bd98592aeda56d38945417d5b0bddedfd681d39
Subhakanta Roul <papu.roul99@gmail.com>
2025-08-30 13:58:22 +0530

    endpoint for getting all stories.

--- server/src/routes/story/story.ts
+++ server/src/routes/story/story.ts
@@ -1,9 +1,11 @@
 import { createStory } from "@/controllers/story/createStory";
+import { getStories } from "@/controllers/story/getStories";
 import { verifyToken } from "@/middleware/verifyToken";
 import { Router } from "express";
 
 const storyRouter: Router = Router();
 
 storyRouter.post("/story", verifyToken, createStory);
+storyRouter.get('/stories', verifyToken, getStories)
 
 export default storyRouter;
