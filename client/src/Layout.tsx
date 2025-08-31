e48515e08fa4036024e230680d33beed76bcd3b0
Subhakanta Roul <papu.roul99@gmail.com>
2025-08-31 22:13:14 +0530

    global layout setup for full screen pages

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -3,14 +3,16 @@ import { type FC, type ReactNode } from 'react';
 import Footer from '@/components/footer/Footer';
 import Navbar from '@/components/navbar/Navbar';
 import Sidebar from '@/components/sidebars/Sidebar';
+import { useSelector } from 'react-redux';
 
 const Layout: FC<{ children: ReactNode }> = ({ children }: { children: ReactNode }) => {
+  const {isAuthRequired} = useSelector((state: any) => state.page);
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Navbar />
       <Box role="main" sx={{ flexGrow: 1, display: 'flex' }}>
-        <Sidebar />
-        <Box sx={{flexGrow: 1}}>{children}</Box>
+        {isAuthRequired && <Sidebar />}
+        <Box sx={{display: 'flex', flexGrow: 1}}>{children}</Box>
       </Box>
       <Footer />
     </Box>
