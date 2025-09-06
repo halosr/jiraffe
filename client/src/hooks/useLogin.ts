f512d64cb89d6ea5b108902deb9fce2964daf8ba
Subhakanta Roul <papu.roul99@gmail.com>
2025-09-06 23:56:16 +0530

    use login hook enhancement to call api

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,16 +1,35 @@
-import { updateLoginFields } from "@/store/actions/login.action";
-import type { RootState, AppDispatch } from "@/store/store";
-import { useDispatch, useSelector } from "react-redux";
+import { login } from '@/api/login';
+import { updateLoginFields } from '@/store/actions/login.action';
+import type { RootState, AppDispatch } from '@/store/store';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const useLogin = () => {
-    const dispatch = useDispatch<AppDispatch>();
-    const {email, password} = useSelector((state: RootState) => state.login || {})
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const { email, password } = useSelector((state: RootState) => state.login || {});
 
-    const handleChange = (field: string, value: string) => {
-        dispatch(updateLoginFields(field, value))
+  const handleChange = (field: string, value: string) => {
+    dispatch(updateLoginFields(field, value));
+  };
+
+  const handleLogin = async () => {
+    try {
+      if (!email || !password) {
+        setError('Missing required fields!!');
+      }
+      const loginResponse = await login(email, password);
+      const {token, uid, name } = loginResponse.data;
+      console.log(token, uid, name);
+      navigate("/rooms")
+    } catch (error) {
+      console.log(error);
+    //   setError(error.message);
     }
-
-    return {email, password, handleChange}
-}
+  };
+  return { email, password, handleChange, handleLogin, error };
+};
 
 export default useLogin;
