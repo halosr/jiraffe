8ed2fac05010439b8e081f202fc0df6f15193d9c
Subhakanta Roul <papu.roul99@gmail.com>
2025-09-19 23:09:38 +0530

    board styles

--- client/src/pages/board/Board.tsx
+++ client/src/pages/board/Board.tsx
@@ -1,5 +1,5 @@
 import { setPageProperties } from '@/store/actions/page.action';
-import { Box, Paper } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -13,8 +13,8 @@ function Board() {
   const estimations = [0, 1, 1, 2, 3, 5, 8, 13, 21];
 
   return (
-    <Box sx={(theme) => ({ display: 'flex',  width: '100vw' })}>
-      <Paper sx={(theme) => ({ display: 'flex', gap: theme.spacing(2),  margin: `${theme.spacing(10)} auto`, })}>
+    <Box sx={(theme) => ({ display: 'flex', width: '100vw' })}>
+      <Paper sx={(theme) => ({ display: 'flex', gap: theme.spacing(2), margin: `${theme.spacing(10)} auto` })}>
         {estimations.map((estimation, index) => (
           <Box
             key={index}
@@ -22,9 +22,16 @@ function Board() {
               background: theme.palette.background.default,
               width: theme.spacing(10),
               height: theme.spacing(15),
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              
             })}
           >
+            <Typography variant='h4' component="h4">
+
             {estimation}
+            </Typography>
           </Box>
         ))}
       </Paper>
