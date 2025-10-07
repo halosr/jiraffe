fee644e5e0fa98e50def7e88a0bec42e2943955c
Subhakanta Roul <papu.roul99@gmail.com>
2025-10-07 23:23:51 +0530

    board component

--- client/src/pages/board/Board.tsx
+++ client/src/pages/board/Board.tsx
@@ -36,7 +36,7 @@ function Board() {
               border: `${theme.spacing(1)} solid ${theme.palette.divider}`,
             })}
           >
-            <Typography variant="h4" component="h3">
+            <Typography variant="h4" component="h4">
               {estimation}
             </Typography>
           </Box>
