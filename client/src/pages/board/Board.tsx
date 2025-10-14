f54f08f1965f4b01cf2b3a81626c105f2e4113e0
Subhakanta Roul <papu.roul99@gmail.com>
2025-10-14 21:40:17 +0530

    board styles

--- client/src/pages/board/Board.tsx
+++ client/src/pages/board/Board.tsx
@@ -36,7 +36,7 @@ function Board() {
               border: `${theme.spacing(1)} solid ${theme.palette.divider}`,
             })}
           >
-            <Typography variant="h3" component="h3">
+            <Typography variant="h5" component="h5">
               {estimation}
             </Typography>
           </Box>
