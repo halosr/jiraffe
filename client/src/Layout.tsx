558bd3d8e8d6f805aa2d7dcda4b378684ad46d9a
Subhakanta Roul <papu.roul99@gmail.com>
2026-01-03 23:40:27 +0530

    client: layout alignment - remove sidebar from application

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -2,21 +2,17 @@ import { Box } from '@mui/material';
 import { type FC, type ReactNode } from 'react';
 import Footer from '@/components/footer/Footer';
 import Navbar from '@/components/navbar/Navbar';
-import Sidebar from '@/components/sidebars/Sidebar';
-import { useSelector } from 'react-redux';
 
 const Layout: FC<{ children: ReactNode }> = ({ children }: { children: ReactNode }) => {
-  const {isAuthRequired} = useSelector((state: any) => state.page);
-  return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Navbar />
-      <Box role="main" sx={{ flexGrow: 1, display: 'flex' }}>
-        {isAuthRequired && <Sidebar />}
-        <Box sx={{display: 'flex', flexGrow: 1}}>{children}</Box>
-      </Box>
-      <Footer />
-    </Box>
-  );
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <Navbar />
+            <Box role="main" sx={{ flexGrow: 1, display: 'flex' }}>
+                <Box width="100%">{children}</Box>
+            </Box>
+            <Footer />
+        </Box>
+    );
 };
 
 export default Layout;
