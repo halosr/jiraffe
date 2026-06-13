c775a177f8160c32bcc4ec488f877e6742b67a30
Subhakanta Roul <papu.roul99@gmail.com>
2026-06-14 00:25:01 +0530

    client: update import alias setup - import from @folder instead of root @ only

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,6 +12,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@theme": path.resolve(__dirname, "./src/theme"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
 })
