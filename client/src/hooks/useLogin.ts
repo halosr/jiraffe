14917349a71d9a5a6226f28ca96abc66409b1af8
Subhakanta Roul <papu.roul99@gmail.com>
2026-06-14 00:23:57 +0530

    client: login using thunk

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,44 +1,45 @@
-import { login } from '@/api/login';
-import { updateLoginFields } from '@/store/actions/login.action';
-import type { RootState, AppDispatch } from '@/store/store';
-import { handleError } from '@/utils/error';
+import { loginUser, updateLoginFields } from '@store/actions/login.action';
+import type { AppDispatch, RootState } from '@store/store';
+import { handleError } from '@utils/error';
 import { AxiosError } from 'axios';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const useLogin = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const navigate = useNavigate();
-  const [error, setError] = useState('');
-  const { email, password } = useSelector((state: RootState) => state.login || {});
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
+    const [error, setError] = useState('');
+    const { email, password } = useSelector((state: RootState) => state.login || {});
 
-  const handleChange = (field: string, value: string) => {
-    setError('');
-    dispatch(updateLoginFields(field, value));
-  };
+    const handleChange = (field: string, value: string) => {
+        setError('');
+        dispatch(updateLoginFields(field, value));
+    };
 
-  const handleLogin = async () => {
-    try {
-      if (!email || !password) {
-        setError('Missing required fields!!');
-        return;
-      }
-      setError('');
-      const loginResponse = await login(email, password);
-      if (loginResponse instanceof AxiosError) {
-        throw loginResponse;
-      }
-      const { token, uid, name } = loginResponse?.data;
-      console.log(token, uid, name);
-      navigate('/dashboard');
-    } catch (error) {
-      console.log(error);
-      const { msg } = handleError(error);
-      setError(msg);
-    }
-  };
-  return { email, password, error, handleChange, handleLogin };
+    const handleLogin = async () => {
+        try {
+            if (!email || !password) {
+                setError('Missing required fields!!');
+                return;
+            }
+            setError('');
+            // const loginResponse = await login(email, password);
+            const loginResponse = await dispatch(loginUser());
+            if (loginResponse instanceof AxiosError) {
+                throw loginResponse;
+            }
+            const { token, uid, name } = loginResponse?.data;
+            // const { token, uid, name } = loginResponse
+            console.log(token, uid, name);
+            navigate('/dashboard');
+        } catch (error) {
+            console.log(error);
+            const { msg } = handleError(error);
+            setError(msg);
+        }
+    };
+    return { email, password, error, handleChange, handleLogin };
 };
 
 export default useLogin;
--- client/src/store/actions/login.action.ts
+++ client/src/store/actions/login.action.ts
@@ -1,11 +1,23 @@
-import * as actions from '@/store/actions/actionTypes';
+import { login } from '@api/login';
+import * as actions from '@store/actions/actionTypes';
+import type { AppDispatch, RootState } from '@store/store';
 
 export function updateLoginFields(field: string, value: string) {
-  return {
-    type: actions.UPDATE_FORM_FIELD,
-    paylaod: {
-      field,
-      value,
-    },
-  };
+    return {
+        type: actions.UPDATE_FORM_FIELD,
+        paylaod: {
+            field,
+            value,
+        },
+    };
+}
+
+export function loginUser() {
+    return async function (dispatch: AppDispatch, getState: () => RootState) {
+        const { email, password } = getState()?.login;
+        return Promise.resolve()
+            .then(() => login(email, password))
+            .then((res) => res?.data)
+            .catch((err) => err);
+    };
 }
