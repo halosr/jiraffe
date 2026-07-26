eeb92aab2c44664d9688222f7084cdf24a1c8dd1
Subhakanta Roul <papu.roul99@gmail.com>
2026-07-27 00:55:28 +0530

    client: fix layout

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -5,9 +5,9 @@ import Navbar from '@/components/navbar/Navbar';
 
 const Layout: FC<{ children: ReactNode }> = ({ children }: { children: ReactNode }) => {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
             <Navbar />
-            <Box role="main" sx={{ flexGrow: 1, display: 'flex' }}>
+            <Box role="main" sx={{ flexGrow: 1, display: 'flex', margin: '0 2rem' }}>
                 <Box width="100%">{children}</Box>
             </Box>
             <Footer />
